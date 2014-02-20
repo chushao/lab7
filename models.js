c1539bc3933805a1817995ba79c7795701ad891c
Chu Shao <cshao@churenshao.com>
2014-02-20 01:10:39 -0800

    lab7 put it together lab

--- models.js
+++ models.js
@@ -3,7 +3,10 @@ var Mongoose = require('mongoose');
 
 
 var ProjectSchema = new Mongoose.Schema({
-  // fields are defined here
+    "title" : { type: String }
+  , "date" : Date
+  , "summary" : { type: String }
+  , "image" : { type: String }
 });
 
 exports.Project = Mongoose.model('Project', ProjectSchema);
